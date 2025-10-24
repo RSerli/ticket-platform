71fcc5199c6b7c2e653f497e1765750484d5c94e
Rserli <riccardoserli.95@gmail.com>
2025-10-24 15:48:29 +0200

    pom update + UI detail user

--- src/main/java/it/prodotto/ticket_platform/controller/indexController.java
+++ src/main/java/it/prodotto/ticket_platform/controller/indexController.java
@@ -1,5 +1,7 @@
 package it.prodotto.ticket_platform.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -8,8 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import it.prodotto.ticket_platform.model.ticket;
 import it.prodotto.ticket_platform.model.user;
 import it.prodotto.ticket_platform.repository.stateRepository;
+import it.prodotto.ticket_platform.repository.ticketRepository;
 import it.prodotto.ticket_platform.repository.userRepository;
 
 
@@ -22,6 +26,9 @@ public class indexController {
 
     @Autowired
     private stateRepository statusRepo;
+
+    @Autowired
+    private ticketRepository ticketRepo;
 
     @GetMapping("/index")
     public String main(Model model) {
@@ -36,9 +43,35 @@ public class indexController {
 
         model.addAttribute("allStatus", statusRepo.findAll());
 
+        // faccio un escursus di tutti i ticket assegnati all'utente e li conto per stato
+
+        List<ticket> finishedTicketsList = ticketRepo.findByUserAssignedAndActualStatus(detailUser, statusRepo.findById(3).get());
+        Integer numberFinishedTickets = finishedTicketsList.size();
+        model.addAttribute ("numberFinishedTickets",numberFinishedTickets);
+
+        List<ticket> toDoTicketsList = ticketRepo.findByUserAssignedAndActualStatus(detailUser, statusRepo.findById(2).get());
+        Integer numberToDoTickets = toDoTicketsList.size();
+        model.addAttribute ("numberToDoTickets",numberToDoTickets);
+
+        List<ticket> toWorkTicketsList = ticketRepo.findByUserAssignedAndActualStatus(detailUser, statusRepo.findById(1).get());
+        Integer numberToWorkTickets = toWorkTicketsList.size();
+        model.addAttribute ("numberToWorkTickets",numberToWorkTickets);
+        
+        // controllo per settare la disponibilità di un utente
+
+        if (numberToDoTickets > 0 || numberToWorkTickets > 0) {
+            detailUser.setAvailable(false);
+        } else {
+            detailUser.setAvailable(true);
+        }
+
+        userRepo.save(detailUser);
 
         return "index";
     }
     
+    // --- FUNZIONI DI UTIILTY ---
+
+
 
 }
--- src/main/java/it/prodotto/ticket_platform/repository/ticketRepository.java
+++ src/main/java/it/prodotto/ticket_platform/repository/ticketRepository.java
@@ -1,10 +1,15 @@
 package it.prodotto.ticket_platform.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import it.prodotto.ticket_platform.model.state;
 import it.prodotto.ticket_platform.model.ticket;
+import it.prodotto.ticket_platform.model.user;
+
+
 
 
 public interface ticketRepository extends JpaRepository<ticket, Integer> {
@@ -12,4 +17,6 @@ public interface ticketRepository extends JpaRepository<ticket, Integer> {
     // query  Custom sul nome COMPLETO dell ticket
     public Optional<ticket> findByNome(String nome);
 
+    // QUERY Custom per raggruppameto ticket a secondo dello stato di lavorazione e utente assegnato
+    public List<ticket> findByUserAssignedAndActualStatus(user userAssigned, state actualStatus);
 }
